79c390f8dbd320d1a6ff2c507a3dad34c76b9f4c
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-07-31 04:33:49 +0200

    Improve the parser

--- src/__tests__/parse.test.js
+++ src/__tests__/parse.test.js
@@ -5,17 +5,19 @@ const parse = require('../parse');
 it('parses type declaration', () => {
   expect(
     parse(`
+    type Foo = Bar
     type Bar = Boolean | String | Number
     type Maybe Number = Nothing | Number
     type Users = List User
-  `)
+    `)
   ).toMatchSnapshot();
 });
 
-// it('parses let declaration', () => {
-//   expect(
-//     parse(`
-//     let foo = 10
-//   `)
-//   ).toMatchSnapshot();
-// });
+it('parses let declaration', () => {
+  expect(
+    parse(`
+    let foo = 10
+    let bar = "Hello world"
+  `)
+  ).toMatchSnapshot();
+});
--- src/parse.js
+++ src/parse.js
@@ -2,138 +2,175 @@
 
 const tokenize = require('./tokenize');
 
-const Program = 'Program';
 const Identifier = 'Identifier';
 const LetDeclaration = 'LetDeclaration';
 const TypeDeclaration = 'TypeDeclaration';
-const CallExpression = 'CallExpression';
+const ParameterExpression = 'ParameterExpression';
 const AssignmentExpression = 'AssignmentExpression';
 const StringLiteral = 'StringLiteral';
 const NumericLiteral = 'NumericLiteral';
 const UnionOperation = 'UnionOperation';
 
+const declaration = (token, peek, end) => {
+  if (token && token.type === 'keyword') {
+    if (token.value === 'type') {
+      const expr = expression(peek(), peek, end);
+
+      return {
+        type: TypeDeclaration,
+        value: expr,
+      };
+    }
+
+    if (token.value === 'let') {
+      const expr = expression(peek(), peek, end);
+
+      return {
+        type: LetDeclaration,
+        value: expr,
+      };
+    }
+  }
+};
+
+const expression = (token, peek, end) => {
+  if (token && token.type === 'string') {
+    return {
+      type: StringLiteral,
+      value: token.value,
+    };
+  }
+
+  if (token && token.type === 'number') {
+    return {
+      type: NumericLiteral,
+      value: parseFloat(token.value),
+    };
+  }
+
+  if (token && token.type === 'identifier') {
+    // Lookahead to determine the type of the expression
+    let next = peek();
+    let expr = {
+      type: Identifier,
+      name: token.value,
+    };
+
+    while (next && next.type === 'identifier') {
+      // If the identifier is followed by more identifiers, it's call expression
+      // For the first identifier, create our expression node
+      // For later identifiers, add them to params
+      expr =
+        expr.type === Identifier
+          ? {
+              type: ParameterExpression,
+              callee: expr,
+              params: [],
+            }
+          : expr;
+
+      expr.params.push({
+        type: Identifier,
+        name: next.value,
+      });
+
+      next = peek();
+    }
+    if (next && (next.type === 'string' || next.type === 'number')) {
+      // If the identifier is followed by a string ot number, it's call expression
+      // Unlike identifiers, these can only be at the end
+      expr =
+        expr.type === Identifier
+          ? {
+              type: ParameterExpression,
+              callee: expr,
+              params: [],
+            }
+          : expr;
+
+      expr.params.push(expression(next, peek, end));
+    }
+
+    if (next && next.type === 'operator') {
+      if (next.value === '=') {
+        // If we encounter the assignment operator, assume assignment expression
+        // Assign the current expression to the left side and continue parsing the right side of assignment
+        expr = {
+          type: AssignmentExpression,
+          left: expr,
+          right: expression(peek(), peek, end),
+        };
+      }
+
+      if (next.value === '|') {
+        // If we encounter the union operator, assume union expression
+        // Add the current expression to the list of values in the union
+        const values = [expr];
+
+        next = peek();
+
+        while (next && next.type !== 'operator') {
+          // Parse the next expression until we find an operator
+          const value = expression(
+            next,
+            () => {
+              // Custom peek implementation so parsing the expression ends at next operator
+              next = peek();
+
+              if (next && next.type === 'operator') {
+                // Returning undefined will finish the parsing
+                return;
+              }
+
+              return next;
+            },
+            end
+          );
+
+          if (value) {
+            values.push(value);
+          } else {
+            break;
+          }
+
+          next = peek();
+        }
+
+        expr = {
+          type: UnionOperation,
+          values,
+        };
+      }
+    }
+
+    if (next && next.type === 'keyword') {
+      // When we encounter a keyword, we end the statement
+      end();
+    }
+
+    return expr;
+  }
+};
+
 module.exports = function parse(code: string) {
-  const root = {
-    type: Program,
-    body: [],
-    parent: {},
-  };
+  const body = [];
 
   const tokens = tokenize(code).filter(
     t => t.type !== 'whitespace' && t.type !== 'newline'
   );
 
-  let current = root;
-  let assignment = false;
-  let union = false;
-
   for (let i = 0, l = tokens.length; i < l; i++) {
     const token = tokens[i];
+    const peek = () => {
+      i++;
+      return tokens[i];
+    };
+    const end = () => {
+      i--;
+    };
 
-    if (token.type === 'keyword') {
-      while (
-        current.parent.type === TypeDeclaration ||
-        current.parent.type === LetDeclaration ||
-        current.parent.type === Program
-      ) {
-        current = current.parent;
-      }
-
-      let expr;
-
-      if (token.value === 'type') {
-        expr = {
-          type: TypeDeclaration,
-          parent: current,
-        };
-      } else if (token.value === 'let') {
-        expr = {
-          type: LetDeclaration,
-          parent: current,
-        };
-      } else {
-        throw new Error(`Invalid keyword ${token.value}`);
-      }
-
-      current.body.push(expr);
-      current = expr;
-    } else if (token.type === 'identifier') {
-      if (current.type === TypeDeclaration || current.type === LetDeclaration) {
-        const expr = {
-          type: Identifier,
-          name: token.value,
-          parent: current,
-        };
-
-        current.id = expr;
-        current = expr;
-      } else if (current.type === Identifier) {
-        const expr = {
-          type: Identifier,
-          name: token.value,
-          parent: current,
-        };
-
-        if (assignment) {
-          current.value = expr;
-          current = expr;
-          assignment = false;
-        } else if (union) {
-          const expr = {
-            type: UnionOperation,
-            types: [],
-            parent: current.parent,
-          };
-
-          expr.types.push(
-            {
-              ...current.value,
-              parent: expr,
-            },
-            {
-              type: Identifier,
-              name: token.value,
-              parent: current,
-            }
-          );
-
-          current.value = expr;
-          current = expr;
-          union = false;
-        } else {
-          current.params = current.params || [];
-          current.params.push({
-            type: Identifier,
-            name: token.value,
-            parent: current,
-          });
-        }
-      } else if (current.type === UnionOperation) {
-        if (union) {
-          current.types.push({
-            type: Identifier,
-            name: token.value,
-            parent: current,
-          });
-          union = false;
-        } else {
-          current = current.parent;
-        }
-      }
-    } else if (token.type === 'operator') {
-      if (current.type === Identifier) {
-        while (current.parent.type === Identifier) {
-          current = current.parent;
-        }
-      }
-
-      if (token.value === '=') {
-        assignment = true;
-      } else if (token.value === '|') {
-        union = true;
-      }
-    }
+    body.push(declaration(token, peek, end) || expression(token, peek, end));
   }
 
-  return root;
+  return body;
 };
